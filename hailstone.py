2476167f1227c11c4fae4ef631f655adcfa79db6
CodeBeyond <dancewaterdance@gmail.com>
2022-01-25 18:33:57 -0800

    1

--- hailstone.py
+++ hailstone.py
@@ -1,12 +1,11 @@
 def hailstone(integer):
     """Integer is the initial number of a hailstone sequence and returns steps it takes to reach 1."""
-    counter = 1
-    while (int(integer)) != 1:
+    counter = 0
+    while integer != 1:
         if integer % 2 == 0:
-            counter += (integer / 2)
+            integer //= 2
         else:
-            counter += ((integer * 3) + 1)
-        return counter
+            integer = 3 * integer + 1
+        counter += 1
+    return counter
 
-    answer = hailstone(10)
-    print(answer)
